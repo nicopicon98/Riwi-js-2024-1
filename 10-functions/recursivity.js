14e3ca2f29a439f45663de9a9ca4ab96fefd6792
Nicolas <nicolaspicon98@outlook.com>
2024-04-19 17:55:40 -0500

    add functions and objects-extension

---
+++ 10-functions/recursivity.js
@@ -0,0 +1,22 @@
+
+
+
+// let ax = 1;
+// const a = function(){
+//     console.log(ax); // shows 1, 2, 3, 4
+//     if(ax === 5){ // condition flag to stop the recursion
+//         console.log("it never entered here");
+//         return;
+//     }
+//     ax++; // increment the flag
+//     a(); // call the function again
+// }
+// a();
+
+// What's the difference between the two snippets, by function naming?
+
+// Basically, the first snippet is a named function, and the second snippet is an anonymous function.
+// The difference is that the named function can be called by its name, and the anonymous function can be called by the variable that holds it.
+// In the first snippet, the function is called by its name, and in the second snippet, 
+// the function is called by the variable that holds it. 
+// The result is the same, but the way to call the function is different.
