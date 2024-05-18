a5be69dc2a99d772e1fc3b0f51a34f3d0258a724
Nicolas Picon <nicolaspicon98@gmail.com>
2024-05-18 14:57:15 -0500

    add 22-file-creations

---
+++ 22-file-creations/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname, 'logs.json')
+const myMessage = {
+    name : "my name",
+    date: Date.now()
+}
+
+fs.appendFile(filePath, JSON.stringify(myMessage), (error) => {
+    if(error){
+        console.log("hubo un error");
+        throw new Error("Error inesperado", error)
+    } else {
+        console.log("se registro correctamente");
+    }
+})
