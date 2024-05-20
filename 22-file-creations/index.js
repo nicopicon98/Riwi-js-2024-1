0efdaeff943d853bd4d9516c2fd7768fc78d49e9
Nicolas Picon <nicolaspicon98@gmail.com>
2024-05-20 08:44:18 -0500

    file-creations update

--- 22-file-creations/index.js
+++ 22-file-creations/index.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 const filePath = path.join(__dirname, 'logs.json')
 const myMessage = {
-    name : "my name",
+    name: "my name",
     date: Date.now()
 }
 
 fs.appendFile(filePath, JSON.stringify(myMessage), (error) => {
-    if(error){
+    if (error) {
         console.log("hubo un error");
         throw new Error("Error inesperado", error)
     } else {
